b0830818b9422ccf55e118f460882ed9314f7dce
peppe7789 <165323825+peppe7789@users.noreply.github.com>
2024-11-22 23:47:12 +0100

    inserito middleware cloud image

--- be-swimpower/routes/user.js
+++ be-swimpower/routes/user.js
@@ -1,8 +1,12 @@
 const express = require('express')
-const users = express.Router()
+const user = express.Router()
 const UserModel = require("../models/UserModel")
 const multer = require("multer")
+const cloudStorage = require("../middleware/updateUserImageCloudinaryMiddleware")
+const ValidateUserBody = require('../middleware/validateUserBody')
+const cloud = multer({ storage : cloudStorage })
 
 
 
-module.exports = users
+
+module.exports = user
