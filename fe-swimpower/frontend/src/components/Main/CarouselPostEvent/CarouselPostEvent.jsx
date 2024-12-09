e04440f99dae6c3c3ce156d40064796fde2942c9
peppe7789 <165323825+peppe7789@users.noreply.github.com>
2024-12-09 21:15:23 +0100

    modificato carousel dimensione

--- fe-swimpower/frontend/src/components/Main/CarouselPostEvent/CarouselPostEvent.jsx
+++ fe-swimpower/frontend/src/components/Main/CarouselPostEvent/CarouselPostEvent.jsx
@@ -2,31 +2,28 @@ import React, { useEffect, useState } from "react";
 import Carousel from "react-bootstrap/Carousel";
 import CarouselSinglePost from "./CarouselSinglePost/CarouselSinglePost";
 import { Col, Container, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { allPostEvents, isPostEventLoading, errorPostEvent, getPostEvents } from "../../../reducer/PostEventSlice"
+
 
 const CarouselPostEvent = () => {
   const [index, setIndex] = useState(0);
-  const [postEvents, setPostEvents] = useState([]);
+  
+  const dispatch = useDispatch()
 
-  const getPostEvent = async () => {
-    try {
-      const response = await fetch(
-        `${import.meta.env.VITE_SWIMPOWER_SERVER_BASE_URL}/postEvent`
-      );
-      const result = await response.json();
+  const postEvents = useSelector(allPostEvents)
+  const isLoading = useSelector(isPostEventLoading)
+  const error = useSelector(errorPostEvent)
 
-      if (Array.isArray(result.postEvent)) {
-        setPostEvents(result.postEvent);
-      } else {
-        setPostEvents([]);
-      }
-    } catch (e) {
-      setPostEvents([]);
-    }
-  };
+ 
+
 
   useEffect(() => {
-    getPostEvent();
-  }, []);
+    dispatch(getPostEvents())
+  }, [dispatch])
+
+
+
 
   const handleSelect = (selectedIndex) => {
     setIndex(selectedIndex);
@@ -40,7 +37,7 @@ const CarouselPostEvent = () => {
           className=" mt-2 pt-4"
         >
           <Col>
-            <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel  activeIndex={index} onSelect={handleSelect}>
               {Array.isArray(postEvents) &&
                 postEvents.slice(0, 5).map((post) => (
                   <Carousel.Item key={post._id}>
--- fe-swimpower/frontend/src/components/Main/CarouselPostEvent/CarouselSinglePost/CarouselSinglePost.jsx
+++ fe-swimpower/frontend/src/components/Main/CarouselPostEvent/CarouselSinglePost/CarouselSinglePost.jsx
@@ -4,7 +4,14 @@ import "./CarouselSinglePost.css"
 const CarouselSinglePost = ({ img }) => {
   return (
     <div className="carousel-single-post d-flex justify-content-center">
-      {img ? <img src={img} alt="imagine" /> : <div>No Image Available</div>}
+      {img
+        ? <img src={img} alt="imagine" style={{
+          width: "100%", 
+          height: "400px", 
+          objectFit: "cover", 
+          borderRadius: "10px",
+        }}/>
+        : <div>No Image Available</div>}
       
     </div>
   )
