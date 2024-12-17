b4584d39d74b2ce2bc132a04127c1acadedd4b9a
peppe7789 <165323825+peppe7789@users.noreply.github.com>
2024-12-17 17:11:40 +0100

    inserimento button

---
+++ fe-swimpower/frontend/src/components/Main/ButtonDeletePost/ButtonDeletePost.jsx
@@ -0,0 +1,24 @@
+import { Tooltip } from "flowbite-react"
+import { AiOutlineDelete } from "react-icons/ai";
+
+
+
+
+
+const ButtonDeletePost = ({onClick}) => {
+    return (
+        <Tooltip content="Elimina Post">
+        <button
+            className="bg-white rounded-2 p-1 text-hover text-reset"
+            onClick={onClick}
+        >
+                <AiOutlineDelete
+                    size={40}
+                />
+                
+        </button>
+    </Tooltip>
+    )
+}
+
+export default ButtonDeletePost
